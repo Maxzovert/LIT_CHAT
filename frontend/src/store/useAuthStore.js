ca37bb0343d3c496a0edb914c82dac9600da5168
Abdullah <95abdullah99@gmail.com>
2024-12-24 18:43:30 +0530

    bug fixing

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -56,6 +56,25 @@ export const useAuthStore = create((set, get) => ({
     }
   },
 
+  
+  //Profile picture update funtion
+  updateProfile : async (data) => {
+    set({isUpdatingProfile : true});
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data ,{
+        withCredentials : true,
+      });
+      set({authUser : res.data});
+      toast.success("Profile Update Successfully")
+    } catch (error) {
+      console.log("Error in  update profile", error);
+      toast.error(error.response.data.message);
+    } finally{
+      set({isUpdatingProfile : false})
+    }
+  },
+
+  
   logout: async () => {
     try {
       await axiosInstance.post("/auth/logout");
@@ -65,23 +84,6 @@ export const useAuthStore = create((set, get) => ({
     } catch (error) {
       toast.error(error.response.data.message);
     }
-  },
-
-//Profile picture update funtion
-updateProfile : async (data) => {
-  set({isUpdatingProfile : true});
-  try {
-      const res = await axiosInstance.put("/auth/update-profile", data ,{
-          withCredentials : true,
-      });
-      set({authUser : res.data});
-      toast.success("Profile Update Successfully")
-  } catch (error) {
-      console.log("Error in  update profile", error);
-      toast.error(error.response.data.message);
-  } finally{
-      set({isUpdatingProfile : false})
-  }
   },
   connectSocket: () => {
     const { authUser } = get();
